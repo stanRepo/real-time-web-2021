dc496f73ccb4014eeb7aa824fd6b649c502e99f6
Stan Ooms <Stanooms96@gmail.com>
2021-04-28 15:45:40 +0200

    added gameObjectExample

--- src/socketIO/components/gameReset.js
+++ src/socketIO/components/gameReset.js
@@ -1,5 +1,6 @@
 exports.reset = function(socket, game, io){
     console.log('Game Resetting')
+    console.log(game)
     game.status = "Game ended. Restart game"
     game.currentBet = undefined;
     socket.emit('gameReset')
