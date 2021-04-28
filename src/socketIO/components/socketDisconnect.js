9d293b71543b766e87c047c039c8d7ac565086b2
Stan Ooms <Stanooms96@gmail.com>
2021-04-28 12:53:46 +0200

    fixed bug
    fixed a bug that caused a freeze in the game when game.thisPlayerTurn left the game

--- src/socketIO/components/socketDisconnect.js
+++ src/socketIO/components/socketDisconnect.js
@@ -1,9 +1,22 @@
-// on disconnect
+// on disconnect:
+// 
+// 1. send a UI update to players in the room
+// 2. if this player had the turn then pass on the turn to the next player
+
+const nextPlayer = require('./nextPlayerTurn.js')
+
 exports.disconnect = function (socket, socketID, readyPlayers, io, game) {
   socket.on("disconnect", () => {
     console.log(`user disconnected ID: ${socketID}`);
 
     io.emit("playerLeft", socketID);
+    delete game.playersInRoom[socketID]
     readyPlayers.pop()
+    console.log(game.status)
+    console.log(game.thisPlayerTurn)
+    console.log(socketID)
+    if(game.thisPlayerTurn === socketID && game.status === "hasStarted"){
+nextPlayer.next(socket, socketID, game, io)
+    }
   });
 };
